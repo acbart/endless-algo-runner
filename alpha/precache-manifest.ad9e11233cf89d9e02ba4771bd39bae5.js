035d407a9c463a0c99660aa3c94f79a1076bc810
Jimmy <jimmyskripchuk@gmail.com>
2020-02-28 23:01:49 -0500

    Updates

---
+++ alpha/precache-manifest.ad9e11233cf89d9e02ba4771bd39bae5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/coding-2-try-phaser-jskripchuk/alphaassets/citations.md"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-jskripchuk/alphaassets/images/background.png"
+  },
+  {
+    "revision": "475001de4261a3cc9e18005973f3bae3",
+    "url": "/coding-2-try-phaser-jskripchuk/alphaassets/images/colors.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-jskripchuk/alphaassets/images/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-jskripchuk/alphaassets/images/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-jskripchuk/alphaassets/images/ship3.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-jskripchuk/alphaassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-jskripchuk/alphaassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-jskripchuk/alphaassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-jskripchuk/alphaassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "6ccb6b1b41d42c754102",
+    "url": "/coding-2-try-phaser-jskripchuk/alphagen.main.50fe1d53e9ef0ae58cad.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-jskripchuk/alphagen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "b4b23ed07dfecf92b8d8bbe6b82ebb2a",
+    "url": "/coding-2-try-phaser-jskripchuk/alphaindex.html"
+  },
+  {
+    "revision": "fc8cefcfb102b35601f09d51ee2512ef",
+    "url": "/coding-2-try-phaser-jskripchuk/alphamanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/coding-2-try-phaser-jskripchuk/alphasw.js"
+  }
+]);
---
+++ alpha/sw.js
@@ -0,0 +1,11 @@
+importScripts("/coding-2-try-phaser-jskripchuk/alphaprecache-manifest.ad9e11233cf89d9e02ba4771bd39bae5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
