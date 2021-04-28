3c7258b441c316e839c03b666875be30912f5f40
Austin Cory Bart <acbart@vt.edu>
2021-04-28 03:57:57 -0400

    Updates

---
+++ final/sw.js
@@ -0,0 +1,11 @@
+importScripts("/endless-algo-runner/finalprecache-manifest.450aaac8fcd4b25dba7ebcbd96642be8.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
